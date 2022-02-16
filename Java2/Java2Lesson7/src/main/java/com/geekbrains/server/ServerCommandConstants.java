f40f9a5c0a12e137b188ab7a2df9068fe7be0ae3
alexey <fant205@mail.ru>
2022-02-16 13:25:38 +0300

    Java 2 Homework 7 - дополнил метод Server.broadcastMessage

---
+++ Java2/Java2Lesson7/src/main/java/com/geekbrains/server/ServerCommandConstants.java
@@ -0,0 +1,7 @@
+package com.geekbrains.server;
+
+public class ServerCommandConstants {
+    public static final String AUTHORIZATION = "/auth";
+    public static final String SHUTDOWN = "/end";
+    public static final String PERSONAL_MESSAGE = "/w";
+}
