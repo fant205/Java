ea18b92160f2dcb847d905e98b7995a01aff8a26
alexey <fant205@mail.ru>
2022-04-08 10:28:39 +0300

    add Java 3 Lesson 7

---
+++ Java3/Java3Lesson7/src/main/java/ru/gb/Homework7.java
@@ -0,0 +1,15 @@
+package ru.gb;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Homework7 {
+
+    private static final Logger LOGGER = LogManager.getLogger(Homework7.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("Hi");
+    }
+
+}
---
+++ Java3/Java3Lesson7/src/test/java/ru/gb/Homework7Test.java
@@ -0,0 +1,14 @@
+package ru.gb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class Homework7Test {
+
+    @Test
+    public void method1(){
+        Assertions.assertEquals(true,true);
+    }
+
+}
